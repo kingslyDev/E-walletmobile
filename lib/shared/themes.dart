9a2fb8dbaff2c643273b1c67b082cd413af127a5
kingslydev <ghalyallcoc@gmail.com>
2024-12-13 23:14:01 +0700

    splashs creen

--- lib/shared/themes.dart
+++ lib/shared/themes.dart
@@ -26,3 +26,11 @@ TextStyle blueTextStyle = GoogleFonts.poppins(
 TextStyle greenTextStyle = GoogleFonts.poppins(
   color: greenColor,
 );
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400; 
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
---
+++ lib/ui/pages/splash_screen.dart
@@ -0,0 +1,24 @@
+import 'package:bankga/shared/themes.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkBackgroundColor,
+      body: Center(
+        child: Container(
+          width: 155,
+          height: 50,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/img_logo.png'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
