91de59cd8b00ad99db320a5253d8adfe2f38f4a9
kingslydev <ghalyallcoc@gmail.com>
2024-12-21 21:30:34 +0700

    bottomnavigationbar

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -8,6 +8,73 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightBackgroundColor,
+      bottomNavigationBar: BottomAppBar(
+        color: whiteColor,
+        shape: CircularNotchedRectangle(),
+        clipBehavior: Clip.antiAlias,
+        notchMargin: 6,
+        elevation: 0,
+        child: BottomNavigationBar(
+          backgroundColor: whiteColor,
+          type: BottomNavigationBarType.fixed,
+          elevation: 0,
+          selectedItemColor: blueColor,
+          unselectedItemColor: blackColor,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          selectedLabelStyle: blueTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
+          ),
+          unselectedLabelStyle: blackTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
+          ),
+          items: [
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_overview.png',
+                width: 20,
+                color: blueColor,
+              ),
+              label: 'Overview',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_history.png',
+                width: 20,
+              ),
+              label: 'History',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_stastitic.png',
+                width: 20,
+              ),
+              label: 'Overview',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_reward.png',
+                width: 20,
+              ),
+              label: 'Overview',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: purpleColor,
+        shape: CircleBorder(),
+        child: Image.asset(
+          'assets/ic_pluscirc.png',
+          width: 24,
+          height: 24,
+          fit: BoxFit.contain,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
