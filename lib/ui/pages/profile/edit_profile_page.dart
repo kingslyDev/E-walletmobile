eca65d553857473be756b3deed3a217a1617e9c1
kingslydev <ghalyallcoc@gmail.com>
2024-12-22 20:58:36 +0700

    page editprofile

--- lib/ui/pages/profile/edit_profile_page.dart
+++ lib/ui/pages/profile/edit_profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:bankga/shared/themes.dart';
+import 'package:bankga/ui/widgets/forms.dart';
 import 'package:flutter/material.dart';
 
 class EditProfilePage extends StatelessWidget {
@@ -7,7 +8,43 @@ class EditProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: greenColor,
+      appBar: AppBar(
+        title: Text('Edit Profile'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: 22,
+        ),
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 30,
+              vertical: 22,
+            ),
+            decoration: BoxDecoration(
+              color: whiteColor,
+              borderRadius: BorderRadius.circular(22),
+            ),
+            child: Center(
+              child: Column(
+                spacing: 16,
+                children: [
+                  CustomFieldText(label: 'Username'),
+                  CustomFieldText(label: 'Full Name'),
+                  CustomFieldText(label: 'Email Address'),
+                  CustomFieldText(
+                    label: 'Password',
+                    obsecure: true,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
